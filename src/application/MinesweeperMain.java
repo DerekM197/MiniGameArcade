7af3eaa613bff199bdf4c18735ef6c03e6ac393d
Chrstian Moore <CMoore@student.neumont.edu>
2017-02-22 09:08:33 -0700

    .

--- src/application/MinesweeperMain.java
+++ src/application/MinesweeperMain.java
@@ -1,16 +1,17 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TitledPane;
+import javafx.stage.Stage;
 
 
 public class MinesweeperMain extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			TitledPane root = FXMLLoader.load(getClass().getResource("MinesweeperGUI.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
