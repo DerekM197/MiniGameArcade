5065b9a6cc91ec541980ee26458ed8ef48d16051
DerekM197 <Derekmikelmartin@gmail.com>
2017-02-23 16:47:02 -0700

    Rows semi Working

--- src/application/SudokuMain.java
+++ src/application/SudokuMain.java
@@ -12,11 +12,12 @@ public class SudokuMain extends Application{
 	public void start(Stage primaryStage) {
 		
 		try {
-			GridPane root = FXMLLoader.load(getClass().getResource("Sudoku.fxml"));
+			GridPane root = FXMLLoader.load(getClass().getResource("SudokuNumbers.fxml"));
 			Scene scene = new Scene(root);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- src/application/SudokuMaster.java
+++ src/application/SudokuMaster.java
@@ -6,22 +6,16 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class SudokuMaster {
-
-
 	
+	public SudokuMaster() {
+	}
 	@FXML
 	public GridPane MainBox;
-	
-	
-	
-	@FXML
-	public GridPane[] AllBoxes; 
-
+	public GridPane[] Boxes;
 	
 	public void initialize(){
 		System.out.println("StuffAndThings");
-		//AllBoxes = MainBox.getChildren().toArray(new GridPane[0]);;
-		System.out.println("GERE");
+		Boxes = MainBox.getChildren().toArray(new GridPane[0]);
 	}
 	public void testing()
 	{
@@ -29,52 +23,98 @@ public class SudokuMaster {
 		System.out.println(this.toString());
 	}
 	
+	/*[0][1][2] [0][1][2] [0][1][2]
+	 *[3][4][5] [3][4][5] [3][4][5]
+	 *[6][7][8] [6][7][8] [6][7][8] 
+	 *
+	 *[0][1][2] [0][1][2] [0][1][2]
+	 *[3][4][5] [3][4][5] [3][4][5]
+	 *[6][7][8] [6][7][8] [6][7][8] 
+	 *
+	 *[0][1][2] [0][1][2] [0][1][2]
+	 *[3][4][5] [3][4][5] [3][4][5]
+	 *[6][7][8] [6][7][8] [6][7][8] 
+	*/
 	
-	public void testing(TextField thi){
-//		System.out.println(BoxTL0o0.getId());
-//		if(BoxTL0o0.getText().length()>1)
-//		{
-//			BoxTL0o0.setText(BoxTL0o0.getText().substring(BoxTL0o0.getText().length()-1));
-//		}
-//		System.out.println(BoxTL0o0.getText());
+	
+	private TextField[][] getRows()
+	{
+		TextField[][] test = new TextField[9][9];
+		GridPane[] boxe =  MainBox.getChildren().toArray(new GridPane[0]);
+		int count = 0;
+		for(int i =0,r=0;i<9;++i,r+=3)
+		{
+			for(int j = 0,k=0;j<9;++k)
+			{
+				if(i!=0&&i/3==0&&k==0)
+				{
+					k+=3;
+				}else if(i!=0&&i/6==0&&k==0)
+				{
+					k+=6;
+				}
+				TextField[] hi = boxe[k].getChildren().toArray(new TextField[0]);
+				for(int p = 0;p<3;++p,++j)
+				{
+					System.out.println("I: "+i+" R: "+r+" J: "+j+" K: "+k+" P: "+p+" C: "+count++);
+					test[i][j] = hi[p+r];
+				
+				}
+				System.out.println("--");
+				
+			}
+		
+			if(r==6)
+			{
+				
+				r=-3;
+			}
+		}
+		return test;
 	}
-	
-	
 	
 	public boolean checkFullBoard()
 	{
+		
+//		System.out.println(getRows());
+		TextField[][] rows = getRows();
+		for(int i = 0;i<9;++i)
+		{
+			for(int j=0;j<9;++j)
+			{
+				System.out.print(rows[i][j].getText()+"|");
+			}
+			System.out.println();
+		}
 		boolean valid = true; 
-		System.out.println("HIA");
-		GridPane[] Boxes = MainBox.getChildren().toArray(new GridPane[0]);
-		System.out.println();
+		/*
+		
+		for(int i = 0;i<9;++i)
+		{//Does all rows
+			if(!checkArrSolution(rows[i]))
+			{
+				valid = false;
+				i = 10;
+			}
+			
+		}
+		
+		
 		for(int i = 0;i<9;++i)
 		{//Does all boxes
 			if(!checkArrSolution(Boxes[i].getChildren().toArray(new TextField[0]))){
 				valid = false;
 				i = 10;
 			}
-		}
-		
-		
+		}	*/
 		return valid;
 	}
 	
 	
+
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public boolean checkArrSolution(TextField[] arr)
-	{
+	private boolean checkArrSolution(TextField[] arr)
+	{//Given an array it will make sure there is 1-9 with no repeats and none missing
 		ArrayList<Integer> ints = new ArrayList<>();
 		for(int i = 1;i<10;++i){
 			ints.add(i);
