4a685fc1b9b3abaeb00fc77b4bd416b283741734
Chrstian Moore <CMoore@student.neumont.edu>
2017-03-03 16:34:35 -0700

    dehjd

--- src/application/MasterMindMain.java
+++ src/application/MasterMindMain.java
@@ -1,22 +1,79 @@
 package application;
 
+import java.io.IOException;
+
+import enums.Colors;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class MasterMindMain extends Application{
+	
+	private int colSize = 4;
+	private int rowSize = 10;
+	private int answerSize;
+	
+//	public MasterMindMain(int colSize, int rowSize){
+//		setColSize(colSize);
+//		setRowSize(rowSize);
+//		setAnswerSize(colSize);
+//	}
+	
+	public int getColSize() {
+		return colSize;
+	}
+
+	public void setColSize(int colSize) {
+		this.colSize = colSize;
+	}
+
+	public int getRowSize() {
+		return rowSize;
+	}
+
+	public void setRowSize(int rowSize) {
+		this.rowSize = rowSize;
+	}
+
+	public int getAnswerSize() {
+		return answerSize;
+	}
+
+	public void setAnswerSize(int answerSize) {
+		this.answerSize = answerSize;
+	}
+
+	public BorderPane createContent() throws IOException{
+		BorderPane screen =  FXMLLoader.load(getClass().getResource("MasterMindMainGUI.fxml"));
+		GridPane board = new GridPane();
+		board.gridLinesVisibleProperty().set(true);
+		for(int	row = 0; row < rowSize; row++){
+			for(int col = 0; col < colSize; col++){
+				board
+			}
+		}
+		board.setAlignment(Pos.CENTER);
+		screen.setCenter(board);
+		return screen;
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		BorderPane hi =  FXMLLoader.load(getClass().getResource("MasterMindMainGUI.fxml"));
-		Scene scene = new Scene(hi, 200, 200);
+		
+		Scene scene = new Scene(createContent());
+		primaryStage.setFullScreen(true);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
 	public static void main(String[] args){
+		
 		launch(args);
 	}
 
--- src/application/MasterMindMainGUIController.java
+++ src/application/MasterMindMainGUIController.java
@@ -4,22 +4,22 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import enums.Colors;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 
-public class MasterMindMainGUIController implements Initializable{
-//	ObservableList<Colors> colors = FXCollecitons
-	
-    @FXML
-    private ChoiceBox<Colors> ColorPiecePicker;
+public class MasterMindMainGUIController implements Initializable {
+	ObservableList<Colors> colors = FXCollections.observableArrayList(Colors.values());
+
+	@FXML
+	private ChoiceBox<Colors> ColorPiecePicker;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-//		ColorPiecePicker.setItems(value);
+		ColorPiecePicker.setValue(colors.get(0));
+		ColorPiecePicker.setItems(colors);
 	}
-    
-    
-    
+
 }
