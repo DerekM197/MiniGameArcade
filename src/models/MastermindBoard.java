21680a1a197ef3f1673e50b49b9d084504d43bce
Kade Stephens <KStephens@student.neumont.edu>
2017-03-06 13:37:33 -0700

    fdsfs

--- src/models/MastermindBoard.java
+++ src/models/MastermindBoard.java
@@ -2,17 +2,17 @@ package models;
 
 public class MastermindBoard {
 
-	private MastermindPiece[] board;
+	private MastermindPiece[][] board;
 	
-	public MastermindBoard(){
-		
+	public MastermindBoard(int rowSize, int colSize){
+		board = new MastermindPiece[rowSize][colSize];
 	}
 	
-	public MastermindPiece[] getBoard(){
+	public MastermindPiece[][] getBoard(){
 		return board;
 	}
 	
-	public void setBoard(MastermindPiece[] board){
+	public void setBoard(MastermindPiece[][] board){
 		this.board = board;
 	}
 	
